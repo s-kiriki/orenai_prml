027eb5f31fa34d06c89da85852809823962547ca
s-kiriki <kiriki@qwintet.com>
2014-11-27 15:51:01 +0900

    演習問題3の回答追加

---
+++ study02/question03.py
@@ -0,0 +1,20 @@
+# coding: UTF-8
+import numpy as np
+import math
+
+p_syouga = np.array([2,5,1])
+p_myouga = np.array([-1,2,3])
+px = np.array([1,1,1])
+
+def distance(a, b):
+    return sum((a-b)**2)
+
+print "distance(p_syouga, px) = %f" % distance(p_syouga, px)
+print "distance(p_myouga, px) = %f" % distance(p_myouga, px)
+
+if distance(p_syouga, px) > distance(p_myouga, px):
+    result = "ミョウガ"
+else:
+    result = "ショウガ"
+
+print "識別結果は『%s』です!!" % result
